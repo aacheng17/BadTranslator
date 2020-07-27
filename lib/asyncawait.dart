ba2880726b22a35a27e589dc42d4504190d376e3
Aaron <aacheng17@gmail.com>
2020-07-27 01:08:19 -0400

    First commit

---
+++ lib/asyncawait.dart
@@ -0,0 +1,106 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:random_words/random_words.dart';
+import 'package:http/http.dart' as http;
+
+class AsyncAwait extends StatefulWidget {
+  @override
+  _AsyncAwaitState createState() => _AsyncAwaitState();
+}
+
+class _AsyncAwaitState extends State<AsyncAwait> {
+  TextEditingController _textEditingController;
+
+  bool _isLoading = false;
+  String _string = "";
+
+  void initState() {
+    super.initState();
+    _textEditingController = TextEditingController(text: "This is going to be translated badly.");
+  }
+
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
+  Future<String> _sendRequest(String s) async {
+    String url = "http://localhost:3000/api/search?q=" + Uri.encodeFull(s);
+    String data = await http.read(url);
+    print(data);
+    return jsonDecode(data)["results"];
+  }
+
+  Future<void> _translate() async {
+    try {
+      String s = _textEditingController.text;
+      print("1:"+s);
+      s = await _sendRequest(s);
+      print("2:"+s);
+      setState(() => _string = s);
+    } catch (e) {
+      print("Error translating: " + e);
+    }
+  }
+
+  void _asyncAction() async {
+    setState(() => _isLoading = true);
+    await _translate();
+    setState(() => _isLoading = false);
+  }
+
+  void _randomize() {
+    String adj = generateAdjective().take(1).elementAt(0).toString();
+    adj = adj[0].toUpperCase() + adj.substring(1);
+    String noun1 = generateNoun().take(1).elementAt(0).toString();
+    String noun2 = generateNoun().take(1).elementAt(0).toString();
+    String punc = ([".",".",".",".",".",".",".",".","!","?"]..shuffle()).first;
+    setState(() => _textEditingController.text = adj + " " + noun1 + " " + noun2 + punc);
+  }
+
+  Widget _buttonRandomize() {
+    return RaisedButton(
+      child: Text("Randomize"),
+      color: Colors.yellow,
+      onPressed: _randomize
+    );
+  }
+
+  Widget _textFieldInput() {
+    return TextField(
+      controller: _textEditingController,
+    );
+  }
+
+  Widget _buttonTranslate() {
+    return _isLoading
+      ? CircularProgressIndicator()
+      : RaisedButton(
+      child: Text("Translate"),
+      color: Colors.red,
+      onPressed: _asyncAction
+    );
+  }
+
+  Widget _textResult() {
+    return Text(_string);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Bad Translator")),
+      body: Center(
+        child: Column(
+          children: [
+            _buttonRandomize(),
+            _textFieldInput(),
+            _buttonTranslate(),
+            _textResult()
+          ]
+        )
+      )
+    );
+  }
+}
